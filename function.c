94761a38444e5a2510986fe2763b0f50ed12bbd5
Fraol123 <fraoltesfaye.ft@gmail.com>
2021-03-13 19:49:27 +0000

    aded the nan function

--- function.c
+++ function.c
@@ -47,3 +47,24 @@ int print_string(va_list list)
 	return (i);
 }
 
+/**
+ * print_non - writes chars after a % if they
+ * don't match anything in the struct
+ * @ch1: the first char passed (always %)
+ * @ch2: the char after the %
+ * Return: 1 if ch2 is a % and 2 is ch2 is anything else
+ */
+int print_nan(char ch1, char ch2)
+{
+	if (ch2 == '%')
+	{
+		write(1, &ch2, 1);
+		return (1);
+	}
+	else
+	{
+		write(1, &ch1, 1);
+		write(1, &ch2, 1);
+		return (2);
+	}
+}
