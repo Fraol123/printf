180ddca9e79b65f15086043101bbc1f54734c7aa
Fraol123 <fraoltesfaye.ft@gmail.com>
2021-03-14 08:42:50 +0000

    printf logic

--- _printf.c
+++ _printf.c
@@ -9,9 +9,38 @@
 int _printf(const char *format, ...)
 {
 	int i, count;
+
 	int (*f)(va_list);
+
 	va_list list;
 
 	if (format == NULL)
 		return (-1);
 	va_start(list, format);
+	if (format == NULL)
+		return (-1);
+	va_start(list, format);
+	i = count = 0;
+	while (format[i] != '\0')
+	{
+		if (format[i] == '%')
+		{
+			if (format[i + 1] == '\0')
+				return (-1);
+			f = get_func(format[i + 1]);
+			if (f == NULL)
+				count += print_nan(format[i], format[i + 1]);
+			else
+				count += f(list);
+			i++;
+		}
+		else
+		{
+			_putchar(format[i]);
+			count++;
+		}
+		i++;
+	}
+	va_end(list);
+	return (count);
+}
