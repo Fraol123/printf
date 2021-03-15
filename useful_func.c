ea6e92adea43f94dd4823ba18e54fd2bed8cb6a7
Fraol123 <fraoltesfaye.ft@gmail.com>
2021-03-15 11:00:48 +0000

    base calculator added

---
+++ useful_func.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ *base_len - finds the length of a number
+ *@base:base to be calculated in
+ *@num: checked
+ *Return:length
+ */
+unsigned int base_len(unsigned int num, int base)
+{
+	unsigned int i;
+
+	for (i = 0; num > 0; i++)
+	{
+		num = num / base;
+	}
+	return (i);
+}
