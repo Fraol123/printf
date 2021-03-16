9479ad9a39fe73639fd36d5a937f5791001528b7
Fraol123 <fraoltesfaye.ft@gmail.com>
2021-03-16 07:15:35 +0000

    corrected return value

--- convert_strings.c
+++ convert_strings.c
@@ -1,10 +1,12 @@
 #include "holberton.h"
+#include <stdio.h>
+#include <string.h>
 /**
  *convert_R- converts a string to root 13
  *@args: A va_list pointing to the string to be converted
  *Return: s
  */
-unsigned int convert_R(va_list args, unsigned  char *s)
+unsigned int convert_R(va_list args,   char *s)
 {
 
 	int i, j;
@@ -12,6 +14,10 @@ unsigned int convert_R(va_list args, unsigned  char *s)
 	char src[] = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ";
 	char dest[] = "nNoOpPqQrRsStTuUvVwWxXyYzZaAbBcCdDeEfFgGhHiIjJkKlLmM";
 
+
+	s = va_arg(args, char *); 
+	if (s == NULL)
+		return(-1);
 
 	for (i = 0; *(s + i); i++)
 	{
@@ -24,5 +30,5 @@ unsigned int convert_R(va_list args, unsigned  char *s)
 			}
 		}
 	}
-	return (s);
+	return (dest[j]);
 }
